e268f91ec6b279d0e2a8313fc8ba8bb6329941fb
Chris Coley <chris.num2@gmail.com>
2013-10-11 19:21:11 -0400

    First steps to login redirect

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
-<?php header('Location: http://frame.codingallnight.com:8080/photorings/home.php') ?>
+<?php
+// Requires
+require_once 'admin/Config.php';
+$config = new Config();
+$libPath = $config->getPathToLibDir();
+require_once($libPath . "Auth/UserAuth.php");
+
+
+// Check if the person is logged in.
+$auth = new UserAuth();
+if ($auth->isLoggedIn($_SESSION['loggedIn'])) {
+    header('Location: http://frame.codingallnight.com:8080/photorings/home.php');
+} else {
+    header('Location: http://frame.codingallnight.com:8080/photorings/login.php');
+}
+?>
