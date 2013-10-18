58ef0444e8fcb58dc1ec4b7cdaa9501c97925a8c
Chris Coley <chris.num2@gmail.com>
2013-10-18 06:20:34 -0400

    Added PhotoRings_DB.php
    Added the PhotoRings_DB class to allow easy creation of PDO connections to the DB.

---
+++ libs/Database/PhotoRings_DB.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Class PDO_DB is a wrapper of the PDO class. It only exists to simplify the
+ * creation of a PDO dataabse object. AKA: I don't want to put the database
+ * access info in every class that needs access to the DB.
+ *
+ * @author Chris Coley
+ */
+class PDO_DB extends PDO {
+    // Database Setup
+    private $hostname = 'localhost';
+    private $database = 'photo_rings';
+    private $username = 'pr';
+    private $password = '5RTQrctz7feTCEcz';
+
+    // PDO settings
+    private $PDO_dsn;
+    private $PDO_emulatePrepares = false;
+    private $PDO_errorMode = PDO::ERRMODE_SILENT;
+
+    /**
+     * Creates a PDO instance representing a connection to a database
+     *
+     * @see PDO::__construct()
+     * @throws PDOException Throws a PDOException if the attempt to connect to the database fails.
+     */
+    public function __construct() {
+        $this->PDO_dsn = "mysql:host=".$this->hostname.";dbname=".$this->database.";charset=utf8";
+        parent::__construct($this->PDO_dsn,
+                            $this->username,
+                            $this->password,
+                            array(
+                                PDO::ATTR_EMULATE_PREPARES => $this->PDO_emulatePrepares,
+                                PDO::ATTR_ERRMODE => $this->PDO_errorMode
+                            ));
+    }
+}
+?>
