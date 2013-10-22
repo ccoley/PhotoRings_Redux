8ec2c40f0c8cbd541e54687507cbbb414460f42e
timothy78775 <tm78775@appstate.edu>
2013-10-22 18:05:54 -0400

    changin branches

---
+++ myPhotos.php
@@ -0,0 +1,44 @@
+<?php
+require_once 'libs/Auth/UserAuth.php';
+$auth = new UserAuth();
+// If the user is not logged in, redirect them to the splash page
+if ($auth->isLoggedIn($_SESSION['loggedIn']) == false) {
+    header("Location: index.php");
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>MyPhotos - PhotoRings</title>
+    <link rel="shortcut icon" href="images/photorings_favicon.ico"/>
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="sidenav/sidenav.css">
+    <!--<link rel="stylesheet" href="css/skeleton.css">-->
+</head>
+<body>
+    <!-- Side navigation -->
+    <div class="sidebar pull-left">
+        <? include 'sidenav/sidenav.html' ?>
+    </div>
+
+    <!-- Main page content -->
+    <div class="main">
+        <div class="container">
+		
+		<div id="threesixty">
+			<div id="spinner">
+				<span>0%</span>
+			</div>
+			
+			<ol id="threesixty_images"></ol>
+		</div>
+				
+        </div>
+    </div>
+
+    <!-- Get them scripts. Load them last to improve page loading speeds. -->
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+</body>
+</html>
